8dea5ace00018cb7d9828ceca6f1a1d44aecf211
WayvshockGD <61085765+WayvshockGD@users.noreply.github.com>
2020-12-03 21:15:34 -0500

    Add files via upload

---
+++ src/main/src/main/java/me/wayv/fire/CommandListener.java
@@ -0,0 +1,12 @@
+package me.wayv.fire;
+
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.entities.Message;
+
+public class CommandListener extends ListenerAdapter
+{
+    public void main(String[] args)
+    {
+
+    }
+}
