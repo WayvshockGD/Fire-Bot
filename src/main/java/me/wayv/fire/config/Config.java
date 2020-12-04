558296354963f0eadbade485dde57ed4cf0aaecb
WayvshockGD <61085765+WayvshockGD@users.noreply.github.com>
2020-12-03 21:18:32 -0500

    Create Config.java

---
+++ src/main/java/me/wayv/fire/config/Config.java
@@ -0,0 +1,19 @@
+package me.wayv.fire.config;
+
+import java.io.IOError;
+
+public class Config
+{
+    public static String Prefix  = "prefix";
+
+    public static String token   = "token";
+
+    public static String ownerID = "ownerID";
+
+    public static String Status  = "Status-Here";
+
+    public void main(String[] args) throws IOError
+    {
+
+    }
+}
